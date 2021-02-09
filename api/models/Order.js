fc8dbaccbd354ff3284c3d1813027621c1995ff1
Pavel Vasylkivskiy <pavel.vasylkivskiy@gmail.com>
2021-02-09 22:43:42 +0200

    add orders delete get post endpoints

--- api/models/Order.js
+++ api/models/Order.js
@@ -13,4 +13,19 @@ module.exports = class Order {
     }).returning('*');
     return result;
   }
+
+  static all = async ({ limit, page }) => {
+    const result = await knex('orders').select().limit(limit).offset((page - 1) * limit);
+    return result;
+  }
+
+  static findById = async  (orderId) => {
+    const result = await knex('orders').where({ id: orderId });
+    return result;
+  }
+
+  static remove = async (orderId) => {
+    const result = await knex('orders').del().where({ id: orderId }).returning('*');
+    return result;
+  }
 }
--- api/routes/orders.js
+++ api/routes/orders.js
@@ -4,8 +4,19 @@ const router = express.Router();
 const Order = require('../models/Order');
 const Product = require('../models/Product');
 
-router.get('/', (req, res, next) => {
-  res.status(200).json({ message: 'Orders were fetched'});
+router.get('/', async (req, res, next) => {
+  const { page, limit } = req.query;
+  try {
+    const orders = await Order.all({ page, limit });
+    res.status(200).json(responseHelper({
+      page,
+      limit,
+      total: orders.length ? orders[0].total : 0,
+      data: orders
+     }))
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post('/', async (req, res, next) => {
@@ -25,14 +36,29 @@ router.post('/', async (req, res, next) => {
   }
 });
 
-router.get('/:orderId', (req, res, next) => {
-  res.status(200).json({
-    message: `You received order with id: ${req.params.orderId}`,
-  });
+router.get('/:orderId', async (req, res, next) => {
+  console.log('main')
+  try {
+    const ordersData = Order.findById(req.params.orderId);
+    if (ordersData.length) {
+      res.status(200).json(responseHelper({ data: ordersData.shift() }));
+    } else {
+      const error = new Error('Not found');
+      error.status = 404;
+      next(error);
+    }
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.delete('/:orderId', (req, res, next) => {
-  res.status(200).json({ message: `You deleted order with id: ${req.params.orderId}`});
+router.delete('/:orderId', async (req, res, next) => {
+  try {
+    const result = await Order.remove(req.params.orderId);
+    res.status(200).json(responseHelper({data: result[0]}));
+  } catch (error) {
+    next(error)
+  }
 });
 
 module.exports = router;
